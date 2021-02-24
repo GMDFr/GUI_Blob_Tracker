eb4d88227d7f502359c8f4d6e872c7fb50cca9d6
Guillaume MORIN-DUPONCHELLE <50672360+GMDFr@users.noreply.github.com>
2021-02-24 09:20:15 +0100

    Add files via upload

---
+++ project/helpers/video_window.py
@@ -0,0 +1,17 @@
+import pyforms
+from pyforms.basewidget import BaseWidget
+from pyforms.controls import ControlPlayer
+
+
+class VideoWindow(BaseWidget):
+
+    def __init__(self):
+
+        BaseWidget.__init__(self, 'Video window')
+
+        self._player = ControlPlayer('Player')
+
+        self.formset = ['_player']
+
+
+if __name__ == "__main__":   pyforms.start_app(VideoWindow)
